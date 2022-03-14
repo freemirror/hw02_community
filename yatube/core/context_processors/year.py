365f3bc6096c33e08c73c5b38eaaf373bfe9890e
Артем Громов <freemirror@ya.ru>
2022-03-14 04:18:29 +0300

    pre preview v

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -1,6 +1,8 @@
 import datetime
+
+
 def year(request):
     now = datetime.datetime.now()
     return {
-        'year':now.year
+        'year': now.year
     }
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,4 +7,5 @@ urlpatterns = [
     path('auth/', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
+    path('about/', include('about.urls', namespace='about')),
 ]
